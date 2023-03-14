3da41e27b725034df2253c8b3758bd496a8d580d
MikhaiLDouG <miixasi2014@yandex.ru>
2023-03-14 12:48:59 +0300

    p19

---
+++ P19/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace P19
+{
+    public class Book
+    {
+        private List<NumberData> numbers;
+        private string path = "PhoneBook.Json";
+        public Book()
+        {
+            numbers = new List<NumberData>();
+            //numbers.Add(new NumberData(1, "Olegov", "Oleg", "Olegovich", 8987, "M Street", "-"));
+            //numbers.Add(new NumberData(2, "Ivanov", "Ivan", "Ivanovich", 8942, "G Street", "-"));
+            //File.WriteAllText("PhoneBook.Json", JsonConvert.SerializeObject(numbers));
+        }
+
+        public List<NumberData> GetNumbers()
+        {
+            string json = File.ReadAllText(path);
+            numbers = JsonConvert.DeserializeObject<List<NumberData>>(json);
+            return numbers;
+        }
+
+        public NumberData GetNumber(int id)
+        {
+            foreach (var i in numbers)
+            {
+                if (i.Id == id)
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ P19/NumberData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P19
+{
+    public class NumberData
+    {
+        private int id;
+        public int Id { get { return id; } }
+
+        private string surname;
+        public string Surname { get { return surname; } }
+
+        private string name;
+        public string Name { get { return name; } }
+
+        private string name2;
+        public string Name2 { get { return name2; } }
+
+        private int phone;
+        public int Phone { get { return phone; } }
+
+        private string address;
+        public string Address { get { return address; } }
+
+        private string description;
+        public string Description { get { return description; } }
+
+        public NumberData(int id, string surname, string name, string name2, int phone, string address, string description)
+        {
+            this.id = id;
+            this.surname = surname;
+            this.name = name;
+            this.name2 = name2;
+            this.phone = phone;
+            this.address = address;
+            this.description = description;
+        }
+    }
+
+}
